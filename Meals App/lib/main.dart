3215978d3bdfc04420e39e89ec579973501a70f8
tauseefsheikh01 <tauseef150198@gmail.com>
2023-09-09 00:52:42 +0530

    Recipe App

--- Meals App/lib/main.dart
+++ Meals App/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:meals/screens/tabs.dart';
 
@@ -13,7 +14,11 @@ final theme = ThemeData(
 );
 
 void main() {
-  runApp(const App());
+  runApp(
+    const ProviderScope(
+      child: App(),
+    ),
+  );
 }
 
 class App extends StatelessWidget {
@@ -22,9 +27,8 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       theme: theme,
       home: const TabsScreen(),
     );
-  } 
+  }
 }
